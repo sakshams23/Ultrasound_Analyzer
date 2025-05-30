8d39a07a33aca3ffc8d7cfb607cfe5336a5c8230
Saksham Soni <sonisaksham20208@gmail.com>
2025-05-31 01:26:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ uploaded_file = st.file_uploader("Choose an ultrasound image...", type=["jpg", "
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert('RGB')
-    st.image(img, caption='Uploaded Image', use_column_width=True)
+    st.image(img, caption='Uploaded Image', use_container_width=True)
 
     img = img.resize((224, 224))
     img_array = image.img_to_array(img)
